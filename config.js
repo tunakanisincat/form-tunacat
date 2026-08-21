7e8f27c8c15f365e51ce8352877899424aa023de
tunakanisincat <tunakanisincat@gmail.com>
2026-08-21 17:49:47 +0900

    Update config.js

--- config.js
+++ config.js
@@ -6,26 +6,32 @@ const CONFIG = {
   webhookUrl: "https://discord.com/api/webhooks/1539928180499161088/CBAQtyLVpGCwvsMYOjArmDnlhi3KLuqeXkYjEm5S5B2l6v9ZVreZ8dap4VP0YHCMjpGl",
 
   questions: [
-    {
+        {
       id: "q1",
+      type: "text", // 'choice' または 'text'
+      title: "名前を入力してください",
+      placeholder: "例：山田 尾場三"
+    },
+    {
+      id: "q2",
       type: "text", // 'choice' または 'text'
       title: "相手を傷つけてしまう言葉には、どんなものがありますか？",
       placeholder: "入力して"
     },
     {
-      id: "q2",
+      id: "q3",
       type: "text", // 'choice' または 'text'
       title: "それを使いたくなるのは、どんな時ですか？",
       placeholder: "入力して"
     },
       {
-      id: "q3",
+      id: "q4",
       type: "text", // 'choice' または 'text'
       title: "それを減らしていくために、どんな方法がありますか？",
       placeholder: "入力して"
     },
       {
-      id: "q4",
+      id: "q5",
       type: "text", // 'choice' または 'text'
       title: "それを聞いたとき、どんな気持ちになりますか？",
       placeholder: "入力して"
